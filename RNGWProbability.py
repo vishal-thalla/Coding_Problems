22835b98b48f2b68a103a678d89ba6488fed8562
Vishal Thalla <vishaxter@gmail.com>
2019-07-15 12:22:33 +0200

    First Daily Coding Challenges Commit

---
+++ RNGWProbability.py
@@ -0,0 +1,36 @@
+# random number generator
+
+import random
+import msvcrt
+
+def populate(probs, counts):
+    
+    return counts
+
+arr = [20, 14, 78, 100, 21, 3, 51, 8]
+probs = [0.13, 0.17, 0.1, 0.156, 0.2 ,0.144, 0.083, 0.017]
+
+prefix = [probs[0]] # this array gives us probability ranges
+
+for i in range(1, len(probs)):
+    prefix.append(probs[i]+prefix[i-1])
+
+counts = [0] *len(arr)
+total = 0
+while True:
+    total +=1
+    rng = random.random()
+    i = 0
+    while i < len(prefix)-1:
+        if rng <= prefix[i]: break
+        i +=1
+    print(arr[i])
+    counts[i] += 1
+
+    if msvcrt.kbhit():
+        for i in counts:
+            print(float(i)/total)
+        break
+
+
+
